2c6341ccdc9db509ee4d8c452fb4018a90938ed3
Jaxcoder <codenamejason@outlook.com>
2022-02-10 22:13:08 -0500

    update name

--- packages/hardhat/contracts/Crystals.sol
+++ packages/hardhat/contracts/Crystals.sol
@@ -18,7 +18,7 @@ contract Crystals is ERC721 {
 
   string [] private uris;
 
-  constructor() public ERC721("Crystals", "CRYSTL") {
+  constructor() public ERC721("AI Crystals Collection", "CRYSTL") {
     _setBaseURI("Chris's Path to Ipfs");
     uris =  ['1.json', '2.json'];
   }
