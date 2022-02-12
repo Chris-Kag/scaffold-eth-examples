8a7191e451ab07d342f75f2313aa62fa9e081b28
Chris Kag <lekag.eth@gmail.com>
2022-02-12 03:43:39 +0200

    Update Crystals.sol

--- packages/hardhat/contracts/Crystals.sol
+++ packages/hardhat/contracts/Crystals.sol
@@ -10,7 +10,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Crystals is ERC721 {
 
-  address payable public constant kag = 0x43dc68536B268Af5C3F454CF75011CE7870f8D1F;
+  address payable public constant chris_kag = 0x43dc68536B268Af5C3F454CF75011CE7870f8D1F;
   address payable public constant jaxcoder = 0x358Aa5Cb060E4bC81C4B9a63CA034c232c054178;
 
   using Counters for Counters.Counter;
@@ -35,7 +35,7 @@ contract Crystals is ERC721 {
     '_119.json', '_120.json', '_121.json', '_122.json', '_123.json', '_124.json', '_125.json', '_126.json', '_127.json', '_128.json',
     '_129.json', '_130.json', '_131.json', '_132.json', '_133.json', '_134.json', '_135.json', '_136.json', '_137.json', '_138.json',
     '_139.json', '_140.json', '_141.json', '_142.json', '_143.json', '_144.json', '_145.json', '_146.json', '_147.json', '_148.json',
-    '_149.json', '_150.json', '_151.json', '_152.json', '_153.json', '_154.json', '_155.json', '_156.json', '_157.json', '_158.json', '_159.json', '_160.json', '_161.json', '_162.json', '_163.json', '_164.json', '_165.json', '_166.json', '_167.json', '_168.json', '_169.json', '_170.json', '_171.json', '_172.json', '_173.json', '_174.json', '_175.json', '_176.json', '_177.json', '_178.json', '_179.json', '_180.json', '_181.json', '_182.json', '_183.json', '_184.json', '_185.json', '_186.json', '_187.json', '_188.json', '_189.json', '_190.json', '_191.json', '_192.json', '_193.json', '_194.json', '_195.json', '_196.json', '_197.json', '_198.json', '_199.json', 
+    '_149.json', '_150.json', '_151.json', '_152.json', '_153.json', '_154.json', '_155.json', '_156.json', '_157.json', '_158.json', '_159.json', '_160.json', '_161.json', '_162.json', '_163.json', '_164.json', '_165.json', '_166.json', '_167.json', '_168.json', '_169.json', '_170.json', '_171.json', '_172.json', '_173.json', '_174.json', '_175.json', '_176.json', '_177.json', '_178.json', '_179.json', '_180.json', '_181.json', '_182.json', '_183.json', '_184.json', '_185.json', '_186.json', '_187.json', '_188.json', '_189.json', '_190.json', '_191.json', '_192.json', '_193.json', '_194.json', '_195.json', '_196.json', '_197.json', '_198.json', '_199.json',
     '_200.json', '_201.json', '_202.json', '_203.json', '_204.json', '_205.json', '_206.json', '_207.json', '_208.json', '_209.json', '_210.json', '_211.json', '_212.json', '_213.json', '_214.json', '_215.json', '_216.json', '_217.json', '_218.json', '_219.json', '_220.json', '_221.json', '_222.json', '_223.json', '_224.json', '_225.json', '_226.json', '_227.json', '_228.json', '_229.json', '_230.json', '_231.json', '_232.json', '_233.json', '_234.json', '_235.json', '_236.json', '_237.json', '_238.json', '_239.json', '_240.json', '_241.json', '_242.json', '_243.json', '_244.json', '_245.json', '_246.json', '_247.json', '_248.json', '_249.json',
     '_250.json', '_251.json', '_252.json', '_253.json', '_254.json', '_255.json', '_256.json', '_257.json', '_258.json', '_259.json', '_260.json', '_261.json', '_262.json', '_263.json', '_264.json', '_265.json', '_266.json', '_267.json', '_268.json', '_269.json', '_270.json', '_271.json', '_272.json', '_273.json', '_274.json', '_275.json', '_276.json', '_277.json', '_278.json', '_279.json', '_280.json', '_281.json', '_282.json', '_283.json', '_284.json', '_285.json', '_286.json', '_287.json', '_288.json', '_289.json', '_290.json', '_291.json', '_292.json', '_293.json', '_294.json', '_295.json', '_296.json', '_297.json', '_298.json', '_299.json',
     '_300.json', '_301.json', '_302.json', '_303.json', '_304.json', '_305.json', '_306.json', '_307.json', '_308.json', '_309.json', '_310.json', '_311.json', '_312.json', '_313.json', '_314.json', '_315.json', '_316.json', '_317.json', '_318.json', '_319.json', '_320.json', '_321.json', '_322.json', '_323.json', '_324.json', '_325.json', '_326.json', '_327.json', '_328.json', '_329.json', '_330.json', '_331.json', '_332.json', '_333.json', '_334.json', '_335.json', '_336.json', '_337.json', '_338.json', '_339.json', '_340.json', '_341.json', '_342.json', '_343.json', '_344.json', '_345.json', '_346.json', '_347.json', '_348.json', '_349.json',
@@ -65,7 +65,7 @@ contract Crystals is ERC721 {
       payable
   {
     require( msg.value >= price, "NOT ENOUGH");
-    (bool success,) = kag.call{ value: price - (price / 100) * 20 }("");
+    (bool success,) = chris_kag.call{ value: price - (price / 100) * 20 }("");
     (bool success1, ) = jaxcoder.call{ value: price - (price / 100) * 80 }("");
     require(success, "could not send to kag");
     require( success1, "could not send to jaxcoder");
